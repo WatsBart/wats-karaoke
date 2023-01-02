f718d2230c08009c5c360e39842e2070961bfb35
WatsBart <watsbart@gmail.com>
2023-01-02 23:43:37 +0100

    Song list page

--- src/pages/songs/index.js
+++ src/pages/songs/index.js
@@ -1,19 +1,33 @@
 import { graphql, Link } from 'gatsby'
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import * as React from 'react'
 import Layout from '../../components/layout'
-import { songLink, songLinkList } from '../../page.module.css'
+import { songLink, songlinkList, songlinkImage, songlinkItem, songlinkText } from '../../page.module.css'
 
 const SongsPage = ({ data: { allWpSong: { edges } } }) => {
   return (
     <Layout pageTitle="Songs of Wats Karaoke">
-      {edges.map((song) => {
-        const songData = song.node.songMeta;
-        return (
-          <div className={songLinkList}>
-            <Link className={songLink} key={song.node.id} to={song.node.slug}>{songData.title} - {songData.singer}</Link> <br />
-          </div>
-        )
-      })}
+      <div className={songlinkList}>
+        {edges.map((song) => {
+          const songData = song.node.songMeta;
+          const thumbnail = getImage(songData.picture.localFile)
+          return (
+            <div className={songlinkItem}>
+              <Link className={songLink} key={song.node.id} to={song.node.slug}>
+                <GatsbyImage
+                  image={thumbnail}
+                  alt=""
+                  className={songlinkImage}
+                  objectFit="scale-down"
+                />
+                <div className={songlinkText}>
+                  {songData.title} - {songData.singer} [{songData.language}]
+                </div>
+              </Link> <br />
+            </div>
+          )
+        })}
+      </div>
     </Layout>
   )
 }
@@ -23,16 +37,24 @@ query MyQuery {
   allWpSong {
     edges {
       node {
-        id
         songMeta {
           title
           singer
+          language
+          picture {
+            localFile {
+              childImageSharp {
+                gatsbyImageData(placeholder: BLURRED)
+              }
+            }
           }
+        }
         slug
+        id
       }
     }
   }
-}  
+}
 `
 
 export default SongsPage
