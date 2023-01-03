d8a2cb22a38ab76c44f15e0a69cb845656d1436a
WatsBart <watsbart@gmail.com>
2023-01-03 18:30:15 +0100

    Added search function to song list

--- src/pages/songs/index.js
+++ src/pages/songs/index.js
@@ -2,13 +2,20 @@ import { graphql, Link } from 'gatsby'
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import * as React from 'react'
 import Layout from '../../components/layout'
-import { songLink, songlinkList, songlinkImage, songlinkItem, songlinkText } from '../../page.module.css'
+import { songLink, songlinkList, songlinkImage, songlinkItem, songlinkText, inputLabel, inputField } from '../../page.module.css'
 
 const SongsPage = ({ data: { allWpSong: { edges } } }) => {
+  const [query,setQuery] = React.useState("")
+  console.log(edges)
   return (
     <Layout pageTitle="Songs of Wats Karaoke">
+      <form>
+        <label className={inputLabel}>Search songs by title</label><br/>
+        <input className={inputField} type="text" value={query} onChange={(event)=>{setQuery(event.target.value);console.log(event.target.value)}}/>
+      </form>
+      <br/>
       <div className={songlinkList}>
-        {edges.map((song) => {
+        {edges.filter((song) => song.node.songMeta.title.toUpperCase().includes(query.toUpperCase())).map((song) => {
           const songData = song.node.songMeta;
           const thumbnail = getImage(songData.picture.localFile)
           return (
@@ -27,6 +34,8 @@ const SongsPage = ({ data: { allWpSong: { edges } } }) => {
             </div>
           )
         })}
+        <div />
+        <div />
       </div>
     </Layout>
   )
